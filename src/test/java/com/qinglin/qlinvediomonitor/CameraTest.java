388d0f4a9824934cf199c5fff74b7960752ee174
gqlhjjjmm <2816022892@qq.com>
2023-05-03 01:36:12 +0800

    test

--- src/test/java/com/qinglin/qlinvediomonitor/CameraTest.java
+++ src/test/java/com/qinglin/qlinvediomonitor/CameraTest.java
@@ -1,10 +1,11 @@
 package com.qinglin.qlinvediomonitor;
 
-import org.bytedeco.javacv.CanvasFrame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.javacv.*;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 
 /**
  * @author by shoulaxiao
@@ -16,14 +17,30 @@ import javax.swing.*;
 public class CameraTest {
 
     public static void main(String[] args) throws FrameGrabber.Exception {
-        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-        grabber.start();
+        try {
+            //VideoInputFrameGrabber grabber = VideoInputFrameGrabber.createDefault(0);
+            OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+            grabber.start();//开始获取摄像头数据
+            final CanvasFrame canvas = new CanvasFrame("摄像头");//新建一个窗口
+            canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            canvas.setAlwaysOnTop(true);
 
-        CanvasFrame canvas = new CanvasFrame("摄像头");
-        canvas.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        while (canvas.isDisplayable()){
-            canvas.showImage(grabber.grab());
+            while (true) {
+                if (!canvas.isDisplayable()) {//窗口是否关闭
+                    grabber.stop();//停止抓取
+                    System.exit(-1);//退出
+                }
+
+                Frame frame = grabber.grab();
+                canvas.showImage(frame);//获取摄像头图像并放到窗口上显示，frame是一帧视频图像
+                Thread.sleep(30);//50毫秒刷新一次图像
+            }
+
+
+        }catch (FrameGrabber.Exception e){
+            e.printStackTrace();
+        }catch (InterruptedException e){
+            e.printStackTrace();
         }
-        grabber.close();
     }
 }
--- src/test/java/com/qinglin/qlinvediomonitor/ImgTest.java
+++ src/test/java/com/qinglin/qlinvediomonitor/ImgTest.java
@@ -1,13 +1,31 @@
 package com.qinglin.qlinvediomonitor;
 
+
 import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacpp.opencv_imgcodecs;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ImgTest {
 
 
-    public static void main(String[] args) {
-        opencv_core.Mat image = opencv_imgcodecs.imread("/home/shoulaxiao/Downloads/607f1f3d68_s.jpg");
+    public static void main(String[] args) throws  Exception, IOException {
+        OpenCVFrameGrabber frameGrabber = new OpenCVFrameGrabber(
+                new File("C:\\Users\\huanjiejie\\Videos\\Captures\\Let Go - Beau Young Prince 2023-02-09 00-44-09.mp4"));
+        frameGrabber.start();
+        Frame origImg = frameGrabber.grab();
 
+        //create canvas frame named 'Demo'
+        final CanvasFrame canvas = new CanvasFrame("Demo");
+
+        //Show image in canvas frame
+        canvas.showImage(origImg);
+
+        //This will close canvas frame on exit
+        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
     }
 }
